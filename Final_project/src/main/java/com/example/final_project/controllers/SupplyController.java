d577aa8adde517d94b1ededc239085c9562e802a
Mutafahu1212 <hussainmustafa097@gmail.com>
2025-11-21 02:01:37 -0500

    remove some code

--- Final_project/src/main/java/com/example/final_project/controllers/SupplyController.java
+++ Final_project/src/main/java/com/example/final_project/controllers/SupplyController.java
@@ -9,25 +9,8 @@ import javafx.scene.control.TableView;
 public class SupplyController {
     private TableView<Supply> table;
 
-    public void initialize(){
-        startRefreshThread();
-    }
 
-    private void startRefreshThread(){
-        Thread refrecher = new Thread(() ->{
-           ObservableList<Supply> lastData = FXCollections.observableArrayList();
-           while(true){
-               ObservableList<Supply> currentData = getSupply();
-               if(!currentData.equals(lastData)){
-                   Platform.runLater(()-> table.setItems(currentData));
-                   lastData = FXCollections.observableArrayList(currentData);
-               }
-               try {Thread.sleep(300);} catch (Exception ignored) {}
-           }
-        });
-        refrecher.setDaemon(true);
-        refrecher.start();
-    }
+
 
     public SupplyController(){
 
