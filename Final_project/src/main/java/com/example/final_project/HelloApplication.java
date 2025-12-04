cd56eeea8fefa46ffa25fd6d01c9eff77fb21d91
DaveenaPatel <daveenampatel@gmail.com>
2025-12-03 19:08:57 -0500

    added financeController because of menu

--- Final_project/src/main/java/com/example/final_project/HelloApplication.java
+++ Final_project/src/main/java/com/example/final_project/HelloApplication.java
@@ -1,6 +1,7 @@
 package com.example.final_project;
 
 import com.example.final_project.controllers.EmployeeController;
+import com.example.final_project.controllers.FinanceController;
 import com.example.final_project.controllers.ItemController;
 import com.example.final_project.view.MenuView;
 import javafx.application.Application;
@@ -14,8 +15,9 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         ItemController itemController = new ItemController();
         EmployeeController employeeController = new EmployeeController();
+        FinanceController financeController = new FinanceController();
 //        SupplyView view = new SupplyView(controller);
-        MenuView view = new MenuView(employeeController, itemController);
+        MenuView view = new MenuView(employeeController, itemController, financeController);
 
 
         Scene scene = new Scene(view, 500, 300);
