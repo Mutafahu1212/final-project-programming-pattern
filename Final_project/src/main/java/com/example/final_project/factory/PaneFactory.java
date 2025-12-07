a3ca7be6c8b6ff7bc97efff41dc8aa05114e2944
DaveenaPatel <daveenampatel@gmail.com>
2025-12-07 12:07:28 -0500

    added buttons

--- Final_project/src/main/java/com/example/final_project/factory/PaneFactory.java
+++ Final_project/src/main/java/com/example/final_project/factory/PaneFactory.java
@@ -1,7 +1,13 @@
 package com.example.final_project.factory;
 
+import com.example.final_project.controllers.EmployeeController;
+import com.example.final_project.controllers.FinanceController;
+import com.example.final_project.controllers.ItemController;
+import com.example.final_project.view.EmployeeView;
+import com.example.final_project.view.MenuView;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
@@ -15,8 +21,38 @@ public class PaneFactory {
         box.getChildren().addAll(child);
         return box;
     }
+
     public static Button createButton(String text){
         return new Button(text);
+    }
+
+    public static Button exitButton(){
+         Button exit = new Button("Exit");
+         exit.setStyle("-fx-background-color: Red");
+         return exit;
+    }
+
+    //crete another buttons for menu options
+    public static Button menuButton(String label){
+        Button button = new Button(label);
+
+        return button;
+    }
+
+    public static Button backButton(){
+        Button button = new Button("BACK");
+        button.setStyle("-fx-background-color: LIGHTSKYBLUE");
+        button.setOnAction(actionEvent -> {
+            ItemController itemController = new ItemController();
+            EmployeeController employeeController = new EmployeeController();
+            FinanceController financeController = new FinanceController();
+            MenuView menuView = new MenuView( employeeController, itemController,financeController);
+            Scene scene = new Scene(menuView, 800, 400);
+//            stage.setTitle("Menu");
+//            stage.setScene(scene);
+//            stage.show();
+        });
+        return button;
     }
 
     public static TextField createTextField(String prompt){
@@ -25,6 +61,7 @@ public class PaneFactory {
         tf.setPrefWidth(100);
         return tf;
     }
+
     public static VBox createVBox(double spacing, Node... child){
         VBox box = new VBox(spacing);
         box.getChildren().addAll(child);
