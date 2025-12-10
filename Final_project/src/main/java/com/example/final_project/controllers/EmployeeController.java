8bf8f8479cf2b7e76427976a16c332a2b5420313
DaveenaPatel <daveenampatel@gmail.com>
2025-12-10 01:03:20 -0500

    Edited EmployeeController

--- Final_project/src/main/java/com/example/final_project/controllers/EmployeeController.java
+++ Final_project/src/main/java/com/example/final_project/controllers/EmployeeController.java
@@ -1,13 +1,40 @@
 package com.example.final_project.controllers;
 
 import com.example.final_project.models.Employee;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
+import java.sql.SQLException;
+
 public class EmployeeController {
-    public EmployeeController() {
-    }
+
+    public EmployeeController() {}
 
     public ObservableList<Employee> getEmployees() {
         return Employee.getAllEmployees();
+    }
+
+    ObservableList<Employee> employeesList = FXCollections.observableArrayList();
+
+    public boolean addEmployee(Employee employee) throws SQLException {
+        boolean result = false;
+        if(Employee.addEmployee(employee)){
+            employeesList.add(employee);
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean removeEmployee(Employee employee) {
+        boolean result = false;
+        if(Employee.deleteEmployee(employee)){
+            employeesList.add(employee);
+            result = true;
+        }
+        return result;
+    }
+
+    public boolean updateEmployee(Employee employee){
+        return Employee.updateEmployee(employee);
     }
 }
